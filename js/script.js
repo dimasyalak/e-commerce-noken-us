1add983e88559ae547c31ef8ce5a7b9f5abb8d4f
dimasyalak <yalakdimas@gmail.com>
2023-03-31 02:39:11 +0900

    Latihan 2

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@ const navbarNav = document.querySelector('.navbar-nav');
 
 // Ketika hamburger menu di clik
 document.querySelector('#hamburger-menu') .onclick = () => {
-	navbarNav.classlist.toggle('active');
+	navbarNav.classList.toggle('active');
 };
+
+//click di luar sidebar untuk menghilangkan navbar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
+	navbarNav.classList.remove('active');
+}
+});
